d05bfe07852761386e5772a92b35fc61cf763672
justuche224 <justuche224@gmail.com>
2025-06-01 09:43:57 +0100

    feat:bank transfer

---
+++ assets/index-4ssHizet.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Caxnq4fP.js";const n=()=>o.jsx("div",{children:"hello"});export{n as component};
--- assets/index-CkVvK-Ye.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CkZ5brE_.js";const n=()=>o.jsx("div",{children:"hello"});export{n as component};
