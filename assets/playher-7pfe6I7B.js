a7a1c90bf3cce941c0c02740a8b2717ea68ba1f5
justuche224 <justuche224@gmail.com>
2025-12-11 08:41:54 +0100

    feat: add visual feedback for image upload in KYCsection

---
+++ assets/playher-7pfe6I7B.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index--Qaz3wr2.js";const e=function(){return o.jsx("div",{children:'Hello "/playher"!'})};export{e as component};
---
+++ assets/settings-CL6dG8GZ.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index--Qaz3wr2.js";const e=function(){return o.jsx("div",{children:'Hello "/dashboard/settings"!'})};export{e as component};
